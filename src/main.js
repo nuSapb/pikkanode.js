d519cc4fd7c88491bde8359e54fea166fbdfec0e
nuSapb <nuhundred@gmail.com>
2018-06-23 12:59:11 +0700

    first commit for pikkanode.js

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const Koa = require('koa')
+const koaBody = require('koa-body')
+const serve = require('koa-static')
+
+const app = new Koa()
+
+const stripPrefix = async (ctx, next) => {
+	if (!ctx.path.startsWith('/-')) {
+		ctx.status = 404
+		return
+	}
+
+	ctx.path = ctx.path.slice(2)
+	await next()
+}
+
+app.use(koaBody({ multipart: true }))
+app.use(require('./route'))
+
+app.use(stripPrefix)
+app.use(serve(path.join(process.cwd(), 'public')))
+
+app.listen(8000)
